2d726ef804ebecdec71d5005a2ae19a4eae3f8bb
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-05-27 14:47:48 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Hehe  from "./components/hehe"
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <Hehe></Hehe>
+    </div>
+    </>
+  )
+}
+
+export default App
